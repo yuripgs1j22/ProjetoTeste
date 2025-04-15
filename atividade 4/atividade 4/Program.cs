548d937df41eabb0d07792fc889eb86a9f65340a
Yuri54978216 <Yuri54978216@edu.df.senac.br>
2025-04-15 09:45:50 -0300

    atividade 4

---
+++ atividade 4/atividade 4/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite uma letra: ");
+        char letra = char.Parse(Console.ReadLine().ToLower());
+
+        if ("aeiou".Contains(letra))
+        {
+            Console.WriteLine("A letra é uma vogal.");
+        }
+        else
+        {
+            Console.WriteLine("A letra não é uma vogal.");
+        }
+    }
+}

61119ea1434cd33b0305cb70935b8e50bd756d44
Yuri54978216 <Yuri54978216@edu.df.senac.br>
2025-04-15 09:44:29 -0300

    atividade 3

---
+++ Atividade 3/Atividade 3/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite sua idade: ");
+        int idade = int.Parse(Console.ReadLine());
+
+        if (idade >= 18)
+        {
+            Console.WriteLine("Você pode votar.");
+        }
+        else
+        {
+            Console.WriteLine("Você não pode votar.");
+        }
+    }
+}

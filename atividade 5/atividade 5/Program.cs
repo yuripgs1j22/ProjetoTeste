e16dc56ca7caf8b7dca895824fafedd956782f1f
Yuri54978216 <Yuri54978216@edu.df.senac.br>
2025-04-15 09:48:09 -0300

    atividade 5

---
+++ atividade 5/atividade 5/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("coloca um numero: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero % 3 == 0)
+        {
+            Console.WriteLine("O número é múltiplo de 3.");
+        }
+        else
+        {
+            Console.WriteLine("O número não é múltiplo de 3.");
+        }
+    }
+}

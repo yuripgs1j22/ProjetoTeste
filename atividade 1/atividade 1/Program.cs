1939a391e2eefc392db8eba2de66b4c81aeae747
Yuri54978216 <Yuri54978216@edu.df.senac.br>
2025-04-15 09:41:25 -0300

    atividade 1

---
+++ atividade 1/atividade 1/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero > 0)
+        {
+            Console.WriteLine("O número é positivo.");
+        }
+        else
+        {
+            Console.WriteLine("O número não é positivo.");
+        }
+    }
+}

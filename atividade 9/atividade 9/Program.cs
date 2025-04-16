c14c989a54eb485d9e918a99d1089eda06d1db0a
Yuri54978216 <Yuri54978216@edu.df.senac.br>
2025-04-16 08:41:52 -0300

    atividade 8 e 9

---
+++ atividade 9/atividade 9/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite a idade da pessoa: ");
+        int idade = Convert.ToInt32(Console.ReadLine());
+
+        if (idade >= 18)
+        {
+            Console.WriteLine("A pessoa é maior de idade.");
+        }
+        else
+        {
+            Console.WriteLine("A pessoa é menor de idade.");
+        }
+    }
+}
